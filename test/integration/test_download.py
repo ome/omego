cfcac32d8c2c18717f1632ae39f7f59e922feb5b
Simon Li <spli@dundee.ac.uk>
2014-03-25 22:03:13 +0000

    Use pytest tmpdir in test_download

--- test/integration/test_download.py
+++ test/integration/test_download.py
@@ -21,44 +21,32 @@
 
 import pytest  # noqa
 
-import os
-import shutil
-import tempfile
-
 from omego.framework import main
 from omego.artifacts import DownloadCommand
 
 
 class TestDownload(object):
 
-    def setup_method(self, method):
-        self.cwd = os.getcwd()
-        self.path = tempfile.mkdtemp("", "download-", ".")
-        self.path = os.path.abspath(self.path)
-        os.chdir(self.path)
+    def setup_class(self):
         self.artifact = 'cpp'
-
-    def teardown_method(self, method):
-        try:
-            shutil.rmtree(self.path)
-        finally:
-            # Return to cwd regardless.
-            os.chdir(self.cwd)
 
     def download(self, *args):
         args = ["download", self.artifact] + list(args)
         main(args=args, items=[("download", DownloadCommand)])
 
-    def testDownloadNoUnzip(self):
-        self.download('--skipunzip')
-        files = os.listdir(self.path)
-        assert len(files) == 1
+    def testDownloadNoUnzip(self, tmpdir):
+        with tmpdir.as_cwd():
+            self.download('--skipunzip')
+            files = tmpdir.listdir()
+            assert len(files) == 1
 
-    def testDownloadUnzip(self):
-        self.download('--unzipargs=-q')
-        files = os.listdir(self.path)
-        assert len(files) == 2
+    def testDownloadUnzip(self, tmpdir):
+        with tmpdir.as_cwd():
+            self.download('--unzipargs=-q')
+            files = tmpdir.listdir()
+            assert len(files) == 2
 
-    def testDownloadUnzipDir(self):
-        self.download('--unzipargs=-q', '--unzipdir', 'OMERO.cpp')
-        assert os.path.isdir('OMERO.cpp')
+    def testDownloadUnzipDir(self, tmpdir):
+        with tmpdir.as_cwd():
+            self.download('--unzipargs=-q', '--unzipdir', 'OMERO.cpp')
+            assert tmpdir.ensure('OMERO.cpp', dir=True)
