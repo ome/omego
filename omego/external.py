da45069e112107f5bf173be86cd2df9bf18c5ab9
Simon Li <spli@dundee.ac.uk>
2014-03-26 11:41:52 +0000

    Added a new external module for running commands

---
+++ omego/external.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import subprocess
+import logging
+import os
+import sys
+
+log = logging.getLogger("omego.external")
+
+
+class RunException(Exception):
+
+    def __init__(self, msg, r, stdout, stderr):
+        super(RunException, self).__init__(msg)
+        self.r = r
+        self.stdout = stdout
+        self.stderr = stderr
+
+    def __str__(self):
+        return '%s\nreturn code:%d\nstdout:%s\nstderr:%s' % (
+            super(RunException, self).__str__(), self.r,
+            self.stdout, self.stderr)
+
+
+class External(object):
+    """
+    Manages the execution of shell and OMERO CLI commands
+    """
+
+    def __init__(self):
+        self.old_env = None
+        self.cli = None
+
+    def setup_omero_cli(self, dir):
+        """
+        Imports the omero CLI module so that commands can be run directly.
+        Note Python does not allow a module to be imported multiple times,
+        so this will only work with a single omero instance.
+
+        This can have several surprisingly effects, so setup_omero_cli()
+        must be explcitly called.
+        """
+        if 'omero.cli' in sys.modules:
+            raise Exception('omero.cli can only be imported once')
+
+        log.debug("Setting up omero CLI")
+        dir = os.path.abspath(dir)
+        lib = os.path.join(dir, "lib", "python")
+        if not os.path.exists(lib):
+            raise Exception("%s does not exist!" % lib)
+        sys.path.insert(0, lib)
+
+        import omero
+        import omero.cli
+
+        log.debug("Using omero CLI from %s", omero.cli.__file__)
+
+        self.cli = omero.cli.CLI()
+        self.cli.loadplugins()
+
+    def setup_previous_omero_env(self, olddir, savevarsfile):
+        """
+        Create a copy of the current environment for interacting with the
+        current OMERO server installation
+        """
+        env = self.get_environment(savevarsfile)
+
+        def addpath(varname, p):
+            if not os.path.exists(p):
+                raise Exception("%s does not exist!" % p)
+            current = env.get(varname)
+            if current:
+                env[varname] = p + os.pathsep + current
+            else:
+                env[varname] = p
+
+        olddir = os.path.abspath(olddir)
+        lib = os.path.join(olddir, "lib", "python")
+        addpath("PYTHONPATH", lib)
+        bin = os.path.join(olddir, "bin")
+        addpath("PATH", bin)
+        self.old_env = env
+
+    def new_omero(self, command):
+        """
+        Runs a command as if from the OMERO command-line without the need
+        for using popen or subprocess.
+        """
+        assert isinstance(command, list)
+        if not self.cli:
+            raise Exception('omero.cli not initialised')
+        log.info("Invoking CLI [current environment]: %s", " ".join(command))
+        self.cli.invoke(command, strict=True)
+
+    def old_omero(self, command):
+        """
+        Runs the omero command-line client with an array of arguments using the
+        old environment
+        """
+        assert isinstance(command, list)
+        if not self.old_env:
+            raise Exception('Old environment not initialised')
+        log.info("Running [old environment]: %s", " ".join(command))
+        self.run('omero', command, self.old_env)
+
+    @staticmethod
+    def run(exe, args, env=None):
+        """
+        Runs an executable with an array of arguments, optionally in the
+        specified environment.
+        Returns stdout and stderr
+        """
+        command = [exe] + args
+        if env:
+            log.info("Executing [custom environment]: %s", " ".join(command))
+        else:
+            log.info("Executing : %s", " ".join(command))
+        proc = subprocess.Popen(
+            command, env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # Use communicate() since wait() may deadlock
+        stdout, stderr = proc.communicate()
+        r = proc.returncode
+
+        if r != 0:
+            raise RunException("Non-zero return code", r, stdout, stderr)
+        return stdout, stderr
+
+    def get_environment(self, filename=None):
+        env = os.environ.copy()
+        if not filename:
+            log.debug("Using original environment")
+            return env
+
+        try:
+            with open(filename, "r") as f:
+                log.info("Loading old environment")
+                for line in f:
+                    key, value = line.strip().split("=", 1)
+                    env[key] = value
+                    log.debug("  %s=%s", key, value)
+        except Exception as e:
+            log.error("Failed to load environment variables from %s: %s",
+                      filename, e)
+            raise e
+        # TODO: Throw a catchable exception
+        return env
+
+    def save_env_vars(self, filename, varnames):
+        try:
+            with open(filename, "w") as f:
+                log.info("Saving environment")
+                for var in varnames:
+                    value = os.environ.get(var, "")
+                    f.write("%s=%s\n" % (var, value))
+                    log.debug("  %s=%s", var, value)
+        except Exception as e:
+            log.error("Failed to save environment variables to %s: %s",
+                      filename, e)
---
+++ test/unit/test_external.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2014 University of Dundee & Open Microscopy Environment
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import pytest
+import mox
+
+import os
+import subprocess
+
+from omego.external import External, RunException
+
+
+class TestExternal(object):
+
+    def setup_method(self, method):
+        self.ext = External()
+        self.mox = mox.Mox()
+        self.envfilename = 'test.env'
+
+    def teardown_method(self, method):
+        self.mox.UnsetStubs()
+
+    def create_dummy_server_dir(self, d):
+        # d must be a pytest tmpdir object
+        d.ensure('lib', 'python', 'omero', 'cli', dir=True)
+        d.ensure('bin', dir=True)
+
+        f = d.join(self.envfilename)
+        f.write('TEST_ENVVAR1=abcde\nTEST_ENVVAR2=1=2=3=4=5\n')
+
+    # def test_setup_omero_cli(self):
+    # Not easily testable since it does a direct import
+
+    def test_setup_previous_omero_env(self, tmpdir):
+        self.create_dummy_server_dir(tmpdir)
+        savevarsfile = str(tmpdir.join(self.envfilename))
+
+        self.ext.setup_previous_omero_env(str(tmpdir), savevarsfile)
+        env = self.ext.old_env
+        assert env['PATH'].split(':', 1)[0] == os.path.join(str(tmpdir), 'bin')
+        assert env['PYTHONPATH'].split(':', 1)[0] == os.path.join(
+            str(tmpdir), 'lib', 'python')
+        assert env['TEST_ENVVAR1'] == 'abcde'
+        assert env['TEST_ENVVAR2'] == '1=2=3=4=5'
+
+    def test_new_omero(self):
+        class MockCli:
+            def invoke(*args, **kwargs):
+                assert args[1:] == (['arg1', 'arg2'], )
+                assert kwargs == {'strict': True}
+
+        self.ext.cli = MockCli()
+        self.ext.new_omero(['arg1', 'arg2'])
+
+    def test_old_omero(self):
+        env = {'TEST': 'test'}
+        self.ext.old_env = env
+        self.mox.StubOutWithMock(self.ext, 'run')
+        self.ext.run('omero', ['arg1', 'arg2'], env).AndReturn(0)
+        self.mox.ReplayAll()
+
+        self.ext.old_omero(['arg1', 'arg2'])
+        self.mox.VerifyAll()
+
+    def test_run(self):
+        class MockProc:
+            def __init__(self):
+                self.returncode = 0
+
+            def communicate(self):
+                return 'ret1', 'ret2'
+
+        env = {'TEST': 'test'}
+        self.mox.StubOutWithMock(subprocess, 'Popen')
+        subprocess.Popen(
+            ['test', 'arg1', 'arg2'], env=env, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE).AndReturn(MockProc())
+        subprocess.Popen(
+            ['fail', 'arg1', 'arg2'], env=env, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE).AndRaise(RunException(
+                'Message', 2, 'ret1', 'ret2'))
+        self.mox.ReplayAll()
+
+        self.ext.run('test', ['arg1', 'arg2'], env)
+        with pytest.raises(RunException) as excinfo:
+            self.ext.run('fail', ['arg1', 'arg2'], env)
+        exc = excinfo.value
+        assert exc.r == 2
+        assert exc.message == 'Message'
+        assert exc.stdout == 'ret1'
+        assert exc.stderr == 'ret2'
+        self.mox.VerifyAll()
+
+    def test_get_environment(self, tmpdir):
+        self.create_dummy_server_dir(tmpdir)
+        savevarsfile = str(tmpdir.join(self.envfilename))
+        env = self.ext.get_environment(savevarsfile)
+        assert env['TEST_ENVVAR1'] == 'abcde'
+        assert env['TEST_ENVVAR2'] == '1=2=3=4=5'
+
+    def test_save_env_vars(self, tmpdir):
+        savevarsfile = str(tmpdir.join(self.envfilename))
+
+        self.mox.StubOutWithMock(os.environ, 'get')
+        os.environ.get('TEST_ENVVAR1', '').AndReturn('abcde')
+        self.mox.ReplayAll()
+
+        self.ext.save_env_vars(savevarsfile, ['TEST_ENVVAR1'])
+        with open(savevarsfile) as f:
+            s = f.read()
+        assert s == 'TEST_ENVVAR1=abcde\n'
+        self.mox.VerifyAll()
