a66f90dcd273e475ba35fb318c85fda6b4ac7ce5
jmoore <josh@glencoesoftware.com>
2015-05-29 09:15:23 +0200

    Add build-generation code

--- omego/cross.py
+++ omego/cross.py
@@ -27,9 +27,37 @@ class CrossCommand(Command):
         super(CrossCommand, self).__init__(sub_parsers)
 
         self.parser.add_argument(
-            "specification",
+            "-n", "--dry-run",
+            action="store_true",
+            help='Print all builds and exit')
+        self.parser.add_argument(
+            "-d", "--describe",
+            action="store_true",
+            help='Print all builds with explanations and exit')
+        self.parser.add_argument(
+            "--output",
+            help='Choose an output directory')
+        self.parser.add_argument(
+            "--overwrite",
+            help='Permit overwriting existing files')
+        self.parser.add_argument(
+            "spec",
+            default=("?",),
+            nargs="+",
             help='Specifies which build target to generate')
 
     def __call__(self, args):
         super(CrossCommand, self).__call__(args)
         self.configure_logging(args)
+
+        builds = set()
+        specs = list(args.spec)
+        for spec in specs:
+            if spec == "*":
+                self.log.debug("Building all...")
+            elif spec == "?":
+                self.log.debug("Building random...")
+            elif spec.startswith("B"):
+                self.log.debug("Building base spec...")
+            elif spec.startswith("S"):
+                self.log.debug("Building server spec...")
