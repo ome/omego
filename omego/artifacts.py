e9a726057483876c08caf936a821119c377713e3
Simon Li <spli@dundee.ac.uk>
2015-07-29 21:59:18 +0100

    Add ability to download an artifact from a direct url (undocumented)
    This is mostly for internal use so its possible to use any of the following without needing additional logic (without this commit the direct server.zip url has to be passed as a separate positional argument):
    
    - omego upgrade --branch OMERO-5.1-latest
    - omego upgrade --branch 5.1
    - omego upgrade --branch http://example.org/OMERO.server-5.1.1.zip

--- omego/artifacts.py
+++ omego/artifacts.py
@@ -35,7 +35,9 @@ class Artifacts(object):
 
     def __init__(self, args):
         self.args = args
-        if re.match('[A-Za-z]\w+-\w+', args.branch):
+        if re.match('\w+://.*zip$', args.branch):
+            self.artifacts = SingleDirectArtifact(args)
+        elif re.match('[A-Za-z]\w+-\w+', args.branch):
             self.artifacts = JenkinsArtifacts(args)
         elif re.match('[0-9]+|latest$', args.branch):
             self.artifacts = ReleaseArtifacts(args)
@@ -313,6 +315,24 @@ class ReleaseArtifacts(object):
                 pass
 
         return dl_icever
+
+
+class SingleDirectArtifact(object):
+    """
+    A URL to a single artifact, for convenience when testing
+    """
+
+    def __init__(self, args):
+        self.args = args
+
+        try:
+            finalurl = fileutils.dereference_url(args.branch)
+            log.debug('Checked %s: %s', args.branch, finalurl)
+        except HTTPError as e:
+            log.error('Invalid URL %s: %s', args.branch, e)
+            raise Stop(20, 'Invalid artifact URL')
+
+        set_artifacts_map(self, [finalurl])
 
 
 class DownloadCommand(Command):
