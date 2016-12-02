4c8fc72961abd0921fc6cd58d26f9dccdfd27b4d
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-12-02 14:35:54 +0000

    fix (HTTP Error 401: Unauthorized)

--- omego/artifacts.py
+++ omego/artifacts.py
@@ -226,7 +226,9 @@ class JenkinsArtifacts(ArtifactsList):
     def read_xml(self, buildurl):
         url = None
         try:
-            url = fileutils.open_url(buildurl + 'api/xml')
+            url = fileutils.open_url(buildurl + 'api/xml',
+                                     self.args.httpuser,
+                                     self.args.httppassword)
             log.debug('Fetching xml from %s code:%d', url.url, url.code)
             if url.code != 200:
                 log.error('Failed to get CI XML from %s (code %d)',
