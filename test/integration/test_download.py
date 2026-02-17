64899ab53ba1e8ebb5b7b47fe90a81e40abda430
pwalczysko <p.walczysko@dundee.ac.uk>
2026-02-17 16:18:57 +0000

    Remove commented-out code

--- test/integration/test_download.py
+++ test/integration/test_download.py
@@ -20,7 +20,6 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 from __future__ import division
-# from past.utils import old_div
 from builtins import object
 import pytest  # noqa
 
@@ -38,75 +37,6 @@ class Downloader(object):
         main("omego", args=args, items=[("download", DownloadCommand)])
 
 
-# class TestDownloadJenkins(Downloader):
-
-#     def setup_class(self):
-#         self.artifact = 'java'
-#         self.branch = 'OMERO-build'
-#         self.ice = '3.6'
-
-#     def testDownloadNoUnzip(self, tmpdir):
-#         with tmpdir.as_cwd():
-#             self.download('--skipunzip', '--branch', self.branch,
-#                           '--ice', self.ice)
-#             files = tmpdir.listdir()
-#             assert len(files) == 1
-
-#     def testDownloadUnzip(self, tmpdir):
-#         with tmpdir.as_cwd():
-#             self.download('--branch', self.branch, '--ice', self.ice)
-#             files = tmpdir.listdir()
-#             assert len(files) == 2
-
-#     def testDownloadUnzipDir(self, tmpdir):
-#         with tmpdir.as_cwd():
-#             self.download('--unzipdir', 'OMERO.java',
-#                           '--branch', self.branch,
-#                           '--ice', self.ice)
-#             expected = old_div(tmpdir, 'OMERO.java')
-#             assert expected.exists()
-#             assert expected.isdir()
-
-#     def testDownloadSym(self, tmpdir):
-#         with tmpdir.as_cwd():
-#             self.download('--branch', self.branch, '--ice', self.ice,
-#                           '--sym', 'auto')
-#             files = tmpdir.listdir()
-#             assert len(files) == 3
-
-#             expected = old_div(tmpdir, 'OMERO.java')
-#             assert expected.exists()
-#             assert expected.isdir()
-
-#             # Part two, if an artifact already exists and is unzipped check
-#             # that a new symlink is created if necessary
-#             self.download('--branch', self.branch, '--ice', self.ice,
-#                           '--sym', 'custom.sym')
-#             files2 = tmpdir.listdir()
-#             files2diff = set(files2).symmetric_difference(files)
-#             assert len(files2diff) == 1
-#             sym2 = files2diff.pop()
-#             assert sym2 == (old_div(tmpdir, 'custom.sym'))
-#             assert sym2.isdir()
-
-#     def testDownloadBuildNumber(self):
-#         # Old Jenkins artifacts are deleted so we can't download.
-#         # Instead assert that an AttributeError is raised.
-#         # This is not ideal since this error could occur for other reasons.
-#         branch = self.branch + ':600'
-#         with pytest.raises(AttributeError) as exc:
-#             self.download('--branch', branch, '--ice', self.ice)
-#         assert 'No artifacts' in exc.value.args[0]
-
-#     def testDownloadList(self, tmpdir):
-#         self.artifact = ''
-#         self.branch = 'latest'
-#         with tmpdir.as_cwd():
-#             self.download('--branch', self.branch)
-#             files = tmpdir.listdir()
-#             assert len(files) == 0
-
-
 class TestDownloadRelease(Downloader):
 
     def setup_class(self):
@@ -122,18 +52,3 @@ class TestDownloadRelease(Downloader):
     def testDownloadNonExistingArtifact(self):
         with pytest.raises(AttributeError):
             self.download('-n', '--release', '5.3', '--ice', '3.3')
-
-
-# class TestDownloadBioFormats(Downloader):
-
-#     def setup_class(self):
-#         self.branch = 'BIOFORMATS-build'
-
-#     def testDownloadJar(self, tmpdir):
-#         self.artifact = 'formats-api'
-#         with tmpdir.as_cwd():
-#             self.download('--branch', self.branch)
-#             files = tmpdir.listdir()
-#             assert len(files) == 1
-#             assert files[0].basename.endswith(".jar")
-#             assert files[0].basename.startswith('formats-api')
